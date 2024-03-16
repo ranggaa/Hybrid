fc52f72c4db4cf746991b4bc2b55e6e5b5894a82
pranga82 <pranga82@gmail.com>
2024-03-16 18:13:03 +0530

    New Class Added with Methods

---
+++ src/test/java/commonFunctions/Demo.java
@@ -0,0 +1,22 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=876,b=987,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=656,b=8,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void sub()
+{
+	int a=76765,b=8798,c;
+	c=a+b;
+	System.out.println(c);
+}
+}
