9db86f94596075c171185e8fedf0d98b9911a2ca
pranga82 <pranga82@gmail.com>
2024-03-16 18:15:17 +0530

    Modified Values in methods

--- src/test/java/commonFunctions/Demo.java
+++ src/test/java/commonFunctions/Demo.java
@@ -3,19 +3,19 @@ package commonFunctions;
 public class Demo {
 public void add()
 {
-	int a=876,b=987,c;
+	int a=54554,b=4564,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void div()
 {
-	int a=656,b=8,c;
+	int a=65675,b=5656,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void sub()
 {
-	int a=76765,b=8798,c;
+	int a=656,b=88,c;
 	c=a+b;
 	System.out.println(c);
 }
